e6165fd874b37d3ac870cb0a1f61b2e32a8f3a1f
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-03-05 02:49:31 -0600

    Deleting associated dogs on owners when owner is deleted

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ApplicationRecord
 include Visible
 
-  has_many :dogs
+  has_many :dogs, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true, length: { minimum: 3 }
