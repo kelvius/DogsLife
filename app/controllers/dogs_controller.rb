f1457b32a07a24abf4d39b6987c4353fe94d9227
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-03-05 02:47:10 -0600

    Added a link for shortcut in deleting dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,6 +5,13 @@ class DogsController < ApplicationController
     redirect_to owner_path(@owner)
   end
 
+  def destroy
+    @owner = Owner.find(params[:owner_id])
+    @dog = @owner.dogs.find(params[:id])
+    @dog.destroy
+    redirect_to owner_path(@owner), status: :see_other
+  end
+
   private
     def dog_params
       params.require(:dog).permit(:name, :breed, :subbreed, :status)
