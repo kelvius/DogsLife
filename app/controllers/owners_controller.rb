6eacfea8e059c2ba6eea69ab311120d0256a4ed4
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-03-04 22:32:06 -0600

    Added initial create params

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,4 +6,18 @@ class OwnersController < ApplicationController
   def show
     @owner = Owner.find(params[:id])
   end
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new(name:"...",address:"...")
+
+    if @owner.save
+      redirect_to @owner
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
