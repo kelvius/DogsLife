9ea019fc720f0d44614428eef2552c7f9996d7a5
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-03-05 00:35:56 -0600

    Updated dog partial collection

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,12 @@
 class DogsController < ApplicationController
+  def create
+    @owner = Owner.find(params[:owner_id])
+    @dog = @owner.dogs.create(dog_params)
+    redirect_to owner_path(@owner)
+  end
+
+  private
+    def dog_params
+      params.require(:dog).permit(:name, :breed, :subbreed)
+    end
 end
