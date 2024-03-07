7bf04d93a06d9877fb76a4d9c77f8ff46fae3915
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-03-06 23:30:58 -0600

    Added a search function in dog list

--- app/controllers/gooddogs_controller.rb
+++ app/controllers/gooddogs_controller.rb
@@ -1,8 +1,23 @@
 class GooddogsController < ApplicationController
 
   def index
-    @gooddogs = Gooddog.all.paginate(page: params[:page])
+    @gooddogs = Gooddog.all
 
+    # Apply search filtering if search parameters are provided
+    if params[:search].present?
+      case params[:category]
+      when 'all'
+        @gooddogs = @gooddogs.joins(:breed).where("gooddogs.name LIKE :search OR breeds.name LIKE :search OR gooddogs.favoritetreat LIKE :search", search: "%#{params[:search]}%")
+      when 'name'
+        @gooddogs = @gooddogs.where("name LIKE ?", "%#{params[:search]}%")
+      when 'breed'
+        @gooddogs = @gooddogs.joins(:breed).where("breeds.name LIKE ?", "%#{params[:search]}%")
+      when 'favoritetreat'
+        @gooddogs = @gooddogs.where("favoritetreat LIKE ?", "%#{params[:search]}%")
+      end
+    end
+
+    @gooddogs = @gooddogs.paginate(page: params[:page])
   end
 
   def show
